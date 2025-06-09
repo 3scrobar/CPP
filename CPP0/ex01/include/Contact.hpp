cd53b0354869dc07b82f2361f5a58f5462ae88ea
VM Macbook <corsica-man@hotmail.fr>
2025-06-09 15:50:02 +0200

    Commit at 2025-06-09 15:50:02

---
+++ CPP0/ex01/include/Contact.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.class.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tle-saut <tle-saut@student.42perpignan>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/09 15:02:43 by tle-saut          #+#    #+#             */
+/*   Updated: 2025/06/09 15:07:58 by tle-saut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+
+class Contact{
+	private:
+		static std::string	_fields_name[5];
+		std::string			_informations[11];
+
+		enum Field {
+			FirstName = 0,
+			LastName,
+			NickName,
+			PhoneNumber,
+			DarkestSecret
+		};
+	public:
+		Contact(void);
+		~Contact(void);
+
+		bool	set_contact();
+		void	get_contact(int index)const;
+};
+
+#endif
