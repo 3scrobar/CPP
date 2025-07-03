0fb0b8eac30beb70a332c53e5c87a4c30b752ca2
VM Macbook <corsica-man@hotmail.fr>
2025-07-03 10:51:29 +0200

    wqef

--- CPP03/ex00/src/ClapTrap.cpp
+++ CPP03/ex00/src/ClapTrap.cpp
@@ -6,17 +6,17 @@
 /*   By: tle-saut <tle-saut@student.42perpignan>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 20:02:46 by tle-saut          #+#    #+#             */
-/*   Updated: 2025/07/02 20:22:04 by tle-saut         ###   ########.fr       */
+/*   Updated: 2025/07/03 10:50:41 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/ClapTrap.hpp"
 
-ClapTrap::ClapTrap(std::string name) : _name(name), _HitPoint(10), _Energy(10), _AttackDamage(0)
+ClapTrap::ClapTrap(std::string name) : _HitPoint(10), _AttackDamage(0), _Energy(10), _name(name)
 {
 	std::cout << "Claptrap se reveille, il s'appelle " << _name;
-	std::cout << " possede " << _HitPoint << "point de vie.\n";
-	std::cout << "Il fait " << _AttackDamage << "degats et a " << _Energy << "points d energy" << std::endl;
+	std::cout << " possede " << _HitPoint << " point de vie.\n";
+	std::cout << "Il fait " << _AttackDamage << " degats et a " << _Energy << " points d energy\n" << std::endl;
 }
 
 ClapTrap::~ClapTrap()
@@ -26,6 +26,32 @@ ClapTrap::~ClapTrap()
 
 void ClapTrap::attack(const std::string& target)
 {
-	std::cout << _name << " cible " << target << "avec une attaque causant " << _AttackDamage << " degats" <<std::endl;
-	
+	if (this->_Energy > 0)
+		std::cout << "ClapTrap " << _name << " attack " << target << " causing  " << _AttackDamage << " point of damage !" <<std::endl;
+	this->_Energy--;
+	if (this->_Energy <= 0)
+		{
+			this->_Energy = 0;
+			std::cout << "ClapTrap " << this->_name << " has depleted energy" << std::endl;
+		}
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "ClapTrap " << this->_name << " take " << amount << " damage." << std::endl;
+	this->_HitPoint -= amount;
+	if (_HitPoint <= 0)
+	{
+		_HitPoint = 0;
+		std::cout << "ClapTrap " << this->_name << " is dead" << std::endl;
+		this->~ClapTrap();
+	}
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	this->_HitPoint += amount;
+	if (this->_HitPoint > 10)
+		this->_HitPoint = 10;
+	std::cout << "ClapTrap " << this->_name << " repaired of " << amount << " points and has " << _HitPoint << " HitPoint" << std::endl;
 }
--- CPP03/ex00/src/main.cpp
+++ CPP03/ex00/src/main.cpp
@@ -6,7 +6,20 @@
 /*   By: tle-saut <tle-saut@student.42perpignan>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 20:02:49 by tle-saut          #+#    #+#             */
-/*   Updated: 2025/07/02 20:02:50 by tle-saut         ###   ########.fr       */
+/*   Updated: 2025/07/03 10:44:24 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../header/ClapTrap.hpp"
+
+int main (void)
+{
+	ClapTrap bob("bob");
+
+	bob.attack("robert");
+	bob.takeDamage(6);
+	bob.beRepaired(15);
+	
+	
+	return (0);
+}
